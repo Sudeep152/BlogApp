b3774d1b7238147e0a516b4957f7c041328b4e64
sudeep152 <sudeepdahiya152@gmail.com>
2021-01-14 16:38:22 +0530

    FragmentDone

--- app/src/main/java/com/sudeep/blogapp/MainActivity.kt
+++ app/src/main/java/com/sudeep/blogapp/MainActivity.kt
@@ -2,12 +2,43 @@ package com.sudeep.blogapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.sudeep.blogapp.ButtomNavigation.Feed_Fragment
+import com.sudeep.blogapp.ButtomNavigation.MessageFragment
+import com.sudeep.blogapp.ButtomNavigation.NotificationFragment
+import com.sudeep.blogapp.ButtomNavigation.ProfileFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val feedFragment=Feed_Fragment()
+        val messageFragment=MessageFragment()
+        val notificationFragment=NotificationFragment()
+        val profileFragment=ProfileFragment()
+
+        setCurrentFragment(feedFragment)
+
+        tabs.setOnNavigationItemSelectedListener {
+            when(it.itemId ){
+                R.id.feed_view -> setCurrentFragment(feedFragment)
+                R.id.message_view -> setCurrentFragment(messageFragment)
+                R.id.notification_view ->setCurrentFragment(notificationFragment)
+                R.id.profile_view -> setCurrentFragment(profileFragment)
+
+            }
+            true
+
+        }
+
 
     }
+    private fun setCurrentFragment(fragment:Fragment)=
+        supportFragmentManager.beginTransaction().apply {
+
+            replace(R.id.frame,fragment)
+                .commit()
+        }
 }
